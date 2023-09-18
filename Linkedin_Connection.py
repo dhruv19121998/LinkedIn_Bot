a2c8f87f2e96809c3d9b24498c921d988dca86cd
Dhruv <64854315+dhruv19121998@users.noreply.github.com>
2023-09-18 14:24:31 +0530

    Update Linkedin_Connection.py

--- Linkedin_Connection.py
+++ Linkedin_Connection.py
@@ -4,71 +4,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.common.exceptions import TimeoutException
-# rest of the code
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 
+class LinkedInBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
 
-# Input Email id/ Username & password
-Username = input("Enter the Username: ")
-Passcode = input("Enter the Password: ")
-Look = input("Enter whatever u wanted to search:")
+    def login(self):
+        self.driver.get('https://www.linkedin.com/home')
+        email_field = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="session_key"]')))
+        email_field.send_keys(self.username + Keys.TAB)
+        password_field = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="session_password"]')))
+        password_field.send_keys(self.password + Keys.ENTER)
+        self.wait.until(EC.title_contains("LinkedIn"))
+        sleep(20)
 
-# Set browser Properties and webdriver-wait 
-driver = webdriver.Chrome()
-driver2 = webdriver.Safari()
-wait = WebDriverWait(driver, 10)
+    def search(self, keyword):
+        search_field = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav-typeahead"]/input')))
+        search_field.send_keys(keyword + Keys.ENTER)
+        people_filter = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-reusables__filters-bar"]/ul/li[1]/button')))
+        people_filter.click()
 
-# Visit LinkedIn
-driver.get('https://www.linkedin.com/home')
+    def connect(self):
+        while True:
+            try:
+                connect_buttons = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'artdeco-button__text')))
+                for button in connect_buttons:
+                    if button.text == 'Connect':
+                        button.click()
+                        try:
+                            classmates_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Classmates']")))
+                            classmates_button.click()
+                            final_connect = self.driver.find_element(By.XPATH,"//span[text()='Connect']")
+                            final_connect.click()
+                            final_send = self.driver.find_element(By.XPATH, "//span[text()='Send']")
+                            final_send.click()
+                        except:
+                            final_send = self.driver.find_element(By.XPATH, "//span[text()='Send']")
+                            final_send.click()
+                        self.driver.execute_script("arguments[0].scrollIntoView();", button)
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                next_button = self.wait.until(EC.presence_of_element_located((By.XPATH, "//button[@aria-label='Next']")))
+                next_button.click()
+            except (StaleElementReferenceException, TimeoutException):
+                continue
 
-# Click on Username/Email_Id field and type
-Email = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="session_key"]')))
-Email.send_keys(Username + Keys.TAB)
+    def quit(self):
+        self.driver.quit()
 
-# Click on Password field and type
-Password = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="session_password"]')))
-Password.send_keys(Passcode + Keys.ENTER)
-
-wait.until(EC.title_contains("LinkedIn"))
-
-sleep(20)  # Wait for 10 seconds
-
-
-# Search
-Search = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="global-nav-typeahead"]/input')))
-Search.send_keys(Look + Keys.ENTER)
-
-# People
-People = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-reusables__filters-bar"]/ul/li[1]/button')))
-People.click()
-
-# Scroll down and press the Connect butto
-
-while True:
-    try:
-        connect_buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'artdeco-button__text')))
-        for button in connect_buttons:
-            if button.text == 'Connect':
-                button.click()
-                try:
-                    # Wait for the "Classmates" button to become clickable
-                    classmates_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Classmates']")))
-                    classmates_button.click()
-                    final_connect = driver.find_element(By.XPATH,"//span[text()='Connect']")
-                    final_connect.click()
-                    final_send = driver.find_element(By.XPATH, "//span[text()='Send']")
-                    final_send.click()
-                except:
-                    final_send = driver.find_element(By.XPATH, "//span[text()='Send']")
-                    final_send.click()
-                    # If the "Classmates" button is not clickable, just continue to the next button
-                driver.execute_script("arguments[0].scrollIntoView();", button)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        next_button = wait.until(EC.presence_of_element_located((By.XPATH, "//button[@aria-label='Next']")))
-        next_button.click()
-    except StaleElementReferenceException:
-        continue
-    except TimeoutException:
-        continue
-
+if __name__ == '__main__':
+    username = input("Enter the Username: ")
+    password = input("Enter the Password: ")
+    keyword = input("Enter whatever u wanted to search:")
+    bot = LinkedInBot(username, password)
+    bot.login()
+    bot.search(keyword)
+    bot.connect()
+    bot.quit()
